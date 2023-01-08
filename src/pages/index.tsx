31beb33c2aa9637f9d142ef9afc724ab0ae46f21
DericksonLossIT <dericksonlossit@gmail.com>
2023-01-08 00:52:11 -0300

    implements bio componment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,6 +14,7 @@ const Home: NextPage = () => {
       </Head>
       <Navbar />
       <Hero />
+      <Bio />
       <Footer />
     </>
   );
@@ -197,5 +198,40 @@ const Hero: React.FC = () => {
         </div>
       </div>
     </main>
+  );
+};
+
+const Bio: React.FC = () => {
+  return (
+    <section className="prose-l prose flex min-w-full flex-col bg-base-200 px-80">
+      <div className="">
+        <h1>Quem Somos?</h1>
+        <p>
+          O Favela Contest é um projeto social realizado de maneira independente
+          e que possui como objetivo impactar positivamente jovens em situação
+          de vulnerabilidade social, especialmente aqueles residentes nas
+          comunidades do Estado, sendo isto feito por meio de eventos em que se
+          busca inserir o skateboarding perante a tal público alvo, bem como o
+          desenvolvimento destes no esporte.
+        </p>
+        <p>
+          Nesse sentido, o Favela visa minimizar as mazelas sociais e ser como
+          um instrumento para o desenvolvimento socioemocional da juventude
+          vulnerável, demonstrando que é possível a socialização entre todos,
+          sem a divisão entre gêneros, com cooperativismo e, ainda, haver a
+          melhora no aprendizado escolar e no condicionamento físico dos jovens,
+          de sua coordenação motora e concentração, através da imersão na
+          cultura do skateboarding.
+        </p>
+        <p>
+          Com esse objetivo, o Favela Contest já realizou duas edições do evento
+          “Favela Contest – O game dos crias” em duas comunidades de Porto
+          Alegre, localizadas nos bairros Bom Jesus e Restinga, os quais
+          proporcionaram à população de tais comunidades atrações no esporte,
+          desafios com premiações, apresentações musicais de artistas locais e
+          alimentação no local do evento.
+        </p>
+      </div>
+    </section>
   );
 };
